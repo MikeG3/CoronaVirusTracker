644eab2ab7784fff3bb7bfcbd5ad7b75f9b88c3a
MikeG3 <harveymichaelgarner@hotmail.com>
2020-07-30 22:46:31 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,14 @@
+//MAIN SCRIPT FILE
+
+//GLOBAL VARIABLES
+var backgroundColor = "#00CDEF";
+var date = new Date();
+
+function showMap(){
+  document.getElementById("map").innerHTML = getMap();
+}//close function strat screen
+
+
+//CODE TO BE EXECUTED
+showMap();
+console.log( document.getElementById("map") );
